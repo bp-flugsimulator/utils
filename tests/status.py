2dc839a8fe129d6be9ee5bfa11e7c0ad32c774f6
Jonas <jonas.meurer@outlook.de>
2017-12-01 16:13:33 +0100

    added unittests for status

--- tests/status.py
+++ tests/status.py
@@ -10,4 +10,44 @@ class TestStatus(unittest.TestCase):
     """
     Test cases for the class Status.
     """
-    pass
+
+    def test_ok_with_string(self):
+        """
+        Testcase with simple string.
+        """
+        status = Status.ok("Hello World")
+        string = '{"status": "ok", "payload": "Hello World"}'
+        self.assertEqual(status.to_json(), string)
+
+    def test_ok_with_array_of_strings(self):
+        """
+        Testcase with simple array of string.
+        """
+        status = Status.ok(["Hello World", "Bye"])
+        string = '{"status": "ok", "payload": ["Hello World", "Bye"]}'
+        self.assertEqual(status.to_json(), string)
+
+    def test_ok_with_array_of_integer(self):
+        """
+        Testcase with simple array of integer.
+        """
+        status = Status.ok([0, 1, 2, 3])
+        string = '{"status": "ok", "payload": [0, 1, 2, 3]}'
+        self.assertEqual(status.to_json(), string)
+
+    def test_ok_with_map_mixed(self):
+        """
+        Testcase with map with mixed objects.
+        """
+        status = Status.ok({"Hello World": "Bye", "Integer": 0})
+        string = '{"status": "ok", "payload": {"Hello World": "Bye", "Integer": 0}}'
+        self.assertEqual(status.to_json(), string)
+
+    @unittest.expectedFailure
+    def test_ok_with_no_serializable(self):
+        """
+        Testcase wich failes because the input object is not
+        serializable.
+        """
+        status = Status.ok(ValueError("This is a value error."))
+        status.to_json()
