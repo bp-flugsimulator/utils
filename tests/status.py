ec5a8772834a8a51dd9539927f9a16eae5270154
Jonas <jonas.meurer@outlook.de>
2017-12-02 18:10:21 +0100

    added __eq__ support

--- tests/status.py
+++ tests/status.py
@@ -3,7 +3,7 @@ Test file for the status module.
 """
 
 import unittest
-from utils.status import Status
+from utils import Status
 
 
 class TestStatus(unittest.TestCase):
@@ -17,7 +17,12 @@ class TestStatus(unittest.TestCase):
         """
         status = Status.ok("Hello World")
         string = '{"status": "ok", "payload": "Hello World"}'
-        self.assertEqual(status.to_json(), string)
+        status_string = Status.from_json(string)
+        status_new = Status.from_json(status.to_json())
+
+        self.assertEqual(status, status_string)
+        self.assertEqual(status, status_new)
+        self.assertEqual(status_string, status_new)
 
     def test_ok_with_array_of_strings(self):
         """
@@ -25,7 +30,12 @@ class TestStatus(unittest.TestCase):
         """
         status = Status.ok(["Hello World", "Bye"])
         string = '{"status": "ok", "payload": ["Hello World", "Bye"]}'
-        self.assertEqual(status.to_json(), string)
+        status_string = Status.from_json(string)
+        status_new = Status.from_json(status.to_json())
+
+        self.assertEqual(status, status_string)
+        self.assertEqual(status, status_new)
+        self.assertEqual(status_string, status_new)
 
     def test_ok_with_array_of_integer(self):
         """
@@ -33,7 +43,12 @@ class TestStatus(unittest.TestCase):
         """
         status = Status.ok([0, 1, 2, 3])
         string = '{"status": "ok", "payload": [0, 1, 2, 3]}'
-        self.assertEqual(status.to_json(), string)
+        status_string = Status.from_json(string)
+        status_new = Status.from_json(status.to_json())
+
+        self.assertEqual(status, status_string)
+        self.assertEqual(status, status_new)
+        self.assertEqual(status_string, status_new)
 
     def test_ok_with_map_mixed(self):
         """
@@ -41,7 +56,64 @@ class TestStatus(unittest.TestCase):
         """
         status = Status.ok({"Hello World": "Bye", "Integer": 0})
         string = '{"status": "ok", "payload": {"Hello World": "Bye", "Integer": 0}}'
-        self.assertEqual(status.to_json(), string)
+        status_string = Status.from_json(string)
+        status_new = Status.from_json(status.to_json())
+
+        self.assertEqual(status, status_string)
+        self.assertEqual(status, status_new)
+        self.assertEqual(status_string, status_new)
+
+    def test_err_with_string(self):
+        """
+        Testcase with simple string.
+        """
+        status = Status.err("Hello World")
+        string = '{"status": "err", "payload": "Hello World"}'
+        status_string = Status.from_json(string)
+        status_new = Status.from_json(status.to_json())
+
+        self.assertEqual(status, status_string)
+        self.assertEqual(status, status_new)
+        self.assertEqual(status_string, status_new)
+
+    def test_err_with_array_of_strings(self):
+        """
+        Testcase with simple array of string.
+        """
+        status = Status.err(["Hello World", "Bye"])
+        string = '{"status": "err", "payload": ["Hello World", "Bye"]}'
+        status_string = Status.from_json(string)
+        status_new = Status.from_json(status.to_json())
+
+        self.assertEqual(status, status_string)
+        self.assertEqual(status, status_new)
+        self.assertEqual(status_string, status_new)
+
+    def test_err_with_array_of_integer(self):
+        """
+        Testcase with simple array of integer.
+        """
+        status = Status.err([0, 1, 2, 3])
+        string = '{"status": "err", "payload": [0, 1, 2, 3]}'
+        status_string = Status.from_json(string)
+        status_new = Status.from_json(status.to_json())
+
+        self.assertEqual(status, status_string)
+        self.assertEqual(status, status_new)
+        self.assertEqual(status_string, status_new)
+
+    def test_err_with_map_mixed(self):
+        """
+        Testcase with map with mixed objects.
+        """
+        status = Status.err({"Hello World": "Bye", "Integer": 0})
+        string = '{"status": "err", "payload": {"Hello World": "Bye", "Integer": 0}}'
+        status_string = Status.from_json(string)
+        status_new = Status.from_json(status.to_json())
+
+        self.assertEqual(status, status_string)
+        self.assertEqual(status, status_new)
+        self.assertEqual(status_string, status_new)
 
     @unittest.expectedFailure
     def test_ok_with_no_serializable(self):
@@ -50,4 +122,13 @@ class TestStatus(unittest.TestCase):
         serializable.
         """
         status = Status.ok(ValueError("This is a value error."))
+        status.to_json()
+
+    @unittest.expectedFailure
+    def test_err_with_no_serializable(self):
+        """
+        Testcase wich failes because the input object is not
+        serializable.
+        """
+        status = Status.err(ValueError("This is a value error."))
         status.to_json()
