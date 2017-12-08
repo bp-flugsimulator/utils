e3b4e06166390472841b651f179149eb7874723d
jonas <jonas.meurer@outlook.de>
2017-12-08 19:01:07 +0100

    update

--- tests/rcp_extra/test_rpc_websockets.py
+++ tests/rcp_extra/test_rpc_websockets.py
@@ -2,17 +2,18 @@
 Test file for rpc websockets.
 """
 
-import unittest
 import asyncio
-import signal
 import json
-import os
-import sys
 import logging
 import multiprocessing
+import os
+import signal
+import sys
+import unittest
 
 import websockets
-from utils import Rpc, RpcReceiver, Command, Status
+
+from utils import Command, Rpc, RpcReceiver, Status
 
 # is on both platforms available
 
@@ -27,11 +28,9 @@ class Server:
     should be received.
     """
 
-    def __init__(self, send, output, loop):
+    def __init__(self, send, output):
         self.send = send
         self.output = output
-        self.loop = loop
-        self.process = None
 
     def run(self):
         """
@@ -41,96 +40,6 @@ class Server:
         -------
             process
         """
-        py_file = os.path.join(
-            os.path.join(os.getcwd(), "scripts"), "test_server.py")
-        logging.debug("Running python script {} as server.".format(py_file))
-        logging.debug("using {} to execute server.".format(sys.executable))
-
-        return asyncio.create_subprocess_exec(
-            sys.executable,
-            py_file,
-            stdout=asyncio.subprocess.PIPE,
-            stdin=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
-        )
-
-    def to_json(self):
-        """
-        Transforms the test set into json.
-
-        Returns
-        -------
-            A string which is json formatted.
-        """
-        return json.dumps({"input": self.send, "output": self.output})
-
-
-@asyncio.coroutine
-def forward_stream_to(source, destination):
-    """
-    Forwards all lines from the source to
-    the destination.
-
-    Arguments
-    ---------
-        source: Source stream
-        destination: Destination stream
-
-    Returns
-    -------
-        None
-    """
-    while True:
-        try:
-            line = yield from source.readline()
-            if not line:
-                break
-            destination.write(line.decode())
-        except:
-            pass
-
-    return None
-
-
-class TestRpcReceiver(unittest.TestCase):
-    """
-    Testcases for the RpcReceiver class.
-    """
-
-    def test_math_add_async(self):
-        """
-        Testing simple math add function with async features.
-        """
-        root = logging.getLogger()
-        root.setLevel(logging.DEBUG)
-
-        ch = logging.StreamHandler(sys.stdout)
-        ch.setLevel(logging.DEBUG)
-        formatter = logging.Formatter(
-            COLOR_TEXT + "[CLIENT] [%(asctime)s]: %(message)s" + COLOR_END,
-            datefmt='%M:%S',
-        )
-        ch.setFormatter(formatter)
-        root.addHandler(ch)
-
-        logging.debug("Fork method: {}".format(
-            multiprocessing.get_start_method()))
-        logging.debug("This Process pid: {}".format(os.getpid()))
-
-        @Rpc.method
-        @asyncio.coroutine
-        def math_add(integer1, integer2):
-            """
-            Simple add function with async.
-
-            Arguments
-            ---------
-                integer1: first operand
-                integer2: second operand
-            """
-            yield from asyncio.sleep(1)
-            res = (integer1 + integer2)
-            return res
 
         if os.name == 'nt':
             loop = asyncio.ProactorEventLoop()
@@ -138,30 +47,38 @@ class TestRpcReceiver(unittest.TestCase):
         else:
             loop = asyncio.get_event_loop()
 
-        # define test set
-        server = Server(
-            [
-                Command("math_add", integer1=1, integer2=2).to_json(),
-            ],
-            [
-                Status.ok(3).to_json(),
-            ],
-            loop,
+        logging.debug("Fork method: {}".format(
+            multiprocessing.get_start_method()))
+        logging.debug("This Process pid: {}".format(os.getpid()))
+        py_file = os.path.join(
+            os.path.join(os.getcwd(), "scripts"), "test_server.py")
+        logging.debug("Running python script {} as server.".format(py_file))
+        logging.debug("using {} to execute server.".format(sys.executable))
+
+        raw_process = asyncio.create_subprocess_exec(
+            sys.executable,
+            py_file,
+            stdout=asyncio.subprocess.PIPE,
+            stdin=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+            loop=loop,
         )
 
         logging.debug("Start child process.")
-        process = loop.run_until_complete(server.run())
+        process = loop.run_until_complete(raw_process)
         logging.debug("Child process spawned with pid {}".format(process.pid))
         logging.debug("This Process pid: {}".format(os.getpid()))
 
         logging.debug("Writing json object to stdin.")
         # transfer test set to process
-        process.stdin.write(server.to_json().encode())
+        process.stdin.write(self.to_json().encode())
         process.stdin.write("\n".encode())
 
         # run instant in background
         asyncio.ensure_future(forward_stream_to(process.stdout, sys.stdout))
         asyncio.ensure_future(forward_stream_to(process.stderr, sys.stdout))
+
+        time.sleep(0.5)
 
         recv = RpcReceiver(
             'ws://127.0.0.1:8750/receive_from_server',
@@ -208,3 +125,91 @@ class TestRpcReceiver(unittest.TestCase):
         logging.debug("Wait for process to close.")
         loop.run_until_complete(process.wait())
         loop.close()
+
+    def to_json(self):
+        """
+        Transforms the test set into json.
+
+        Returns
+        -------
+            A string which is json formatted.
+        """
+        return json.dumps({"input": self.send, "output": self.output})
+
+
+@asyncio.coroutine
+def forward_stream_to(source, destination):
+    """
+    Forwards all lines from the source to
+    the destination.
+
+    Arguments
+    ---------
+        source: Source stream
+        destination: Destination stream
+
+    Returns
+    -------
+        None
+    """
+    while True:
+        try:
+            line = yield from source.readline()
+            if not line:
+                break
+            destination.write(line.decode())
+        except:
+            pass
+
+    return None
+
+
+class TestRpcReceiver(unittest.TestCase):
+    """
+    Testcases for the RpcReceiver class.
+    """
+    @classmethod
+    def setUpClass(cls):
+        super(TestRpcReceiver, cls).setUpClass()
+
+        root = logging.getLogger()
+        root.setLevel(logging.DEBUG)
+
+        ch = logging.StreamHandler(sys.stdout)
+        ch.setLevel(logging.DEBUG)
+
+        formatter = logging.Formatter(
+            COLOR_TEXT + "[CLIENT] [%(asctime)s]: %(message)s" + COLOR_END,
+            datefmt='%M:%S',
+        )
+        ch.setFormatter(formatter)
+        root.addHandler(ch)
+
+    def test_math_add_async(self):
+        """
+        Testing simple math add function with async features.
+        """
+
+        @Rpc.method
+        @asyncio.coroutine
+        def math_add(integer1, integer2):
+            """
+            Simple add function with async.
+
+            Arguments
+            ---------
+                integer1: first operand
+                integer2: second operand
+            """
+            yield from asyncio.sleep(1)
+            res = (integer1 + integer2)
+            return res
+
+        server = Server(
+            [
+                Command("math_add", integer1=1, integer2=2).to_json(),
+            ],
+            [
+                Status.ok(3).to_json(),
+            ],
+        )
