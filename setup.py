4ce80f5d66e2b1bfd87236fa11b1d8e6a7615a3c
jonas <jonas.meurer@outlook.de>
2018-03-01 13:21:34 +0100

    fixed verison

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ WEBSOCKETS_REQUIRES = get_requirements("requirements_websockets.txt")
 setup(
     name="bp-flugsimulator-utils",
     description="Utils for the bp-flugsimulator",
-    version="2.1",
+    version="1.0.0",
     scripts=[],
     url="https://github.com/bp-flugsimulator/utils",
     author="bp-flugsimulator",
